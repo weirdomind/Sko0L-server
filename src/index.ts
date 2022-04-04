894774bd0b67583a19efe2c2e094e6be53c4e858
Mind0Bender <mrcircuit1234@gmail.com>
2022-04-04 19:36:35 +0530

    [FIX] cors

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,17 @@ const app = express();
 
 // Middlewares
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    ISDEV ? "http://localhost:8080" : "https://mysko0l-api.herokuapp.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
