89d0be56c362d3b4302151d42958aab2b99b47af
Mind0Bender <mrcircuit1234@gmail.com>
2022-04-04 19:10:04 +0530

    [FIX] cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import AuthRouter from "./auth";
 import UtilsRouter from "./utils";
 const router = Router();
 
 router.use("/auth", AuthRouter);
 router.use("/utils", UtilsRouter);
+router.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  console.log("test");
+  res.send("test");
+});
 
 export default router;
